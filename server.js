83ebff071685c0d73bf7e3b7dc2a15fb9c99cb1c
ge-em <martenglenn@gmail.com>
2020-02-04 22:15:34 +0800

    adding clear queue command (function)

--- server.js
+++ server.js
@@ -77,6 +77,15 @@ const commands = {
 		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
 		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
 	},
+	'clear': (msg) => {
+		if (queue[msg.guild.id] === undefined) {
+			msg.channel.sendMessage(`There are no queue songs need to be cleared`);
+		}
+		if (queue != undefined){
+			queue = {};
+			msg.channel.sendMessage(`Queue Cleared!`);
+		}
+		},
 	'help': (msg) => {
 		let tosend = ['```xl', tokens.prefix + 'join : "Join Voice channel of msg sender"',	tokens.prefix + 'add : "Add a valid youtube link to the queue"', tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause : "pauses the music"',	tokens.prefix + 'resume : "resumes the music"', tokens.prefix + 'skip : "skips the playing song"', tokens.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
 		msg.channel.sendMessage(tosend.join('\n'));
